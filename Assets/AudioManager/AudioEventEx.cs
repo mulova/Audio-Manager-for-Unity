99821d875bb9fa84142d709a606d4b1f4743b5a8
mulova <mulova@gmail.com>
2022-02-15 10:25:02 +0900

    AudioEventEx

---
+++ Assets/AudioManager/AudioEventEx.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Audio
+{
+    public static class AudioEventEx
+    {
+        private static GameObject uiEmitter;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init()
+        {
+            uiEmitter = null;
+        }
+
+        public static void Play(this AudioEvent audio)
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+            if (uiEmitter == null)
+            {
+                uiEmitter = new GameObject("Ui AudioSource");
+                GameObject.DontDestroyOnLoad(uiEmitter);
+            }
+            Play(audio, uiEmitter);
+        }
+
+        public static void Play(this AudioEvent audio, GameObject emitter)
+        {
+            if (audio != null)
+            {
+                AudioManager.PlayEvent(audio, emitter);
+            }
+            else
+            {
+                Debug.LogWarning("audio not assigned");
+            }
+        }
+    }
+}
